dcb01f11bd743083321ee37d95559fd94dbfff98
ossamaelidrissi <ossamaidrissi5@gmail.com>
2022-08-21 15:10:57 +0200

    add router to header

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import Head from 'next/head'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import ExcitedToWorkToGether from '../components/ExcitedToWorkToGether'
+import AboutBanner from '../components/AboutBanner'
+import AboutInfo from '../components/AboutInfo'
+
 
 function About() {
   return (
-    <div>about</div>
+    <div className='font-tajwal h-screen relative overflow-x-hidden' >
+         <Head>
+            <title>Codata Agency</title>
+            <link rel="icon" href="/favicon.ico" />
+            <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet' />
+            <link href="https://fonts.googleapis.com/css2?family=Tajawal&display=swap" rel="stylesheet"></link>
+        </Head>
+
+        <Header />
+
+        <main className=' container mx-auto space-y-10 md:space-y-5' >
+            <AboutBanner />
+            <AboutInfo />
+        </main>
+
+        <ExcitedToWorkToGether />
+        <Footer />
+    </div>
   )
 }
 
